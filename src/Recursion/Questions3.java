947c5f2516777444cac4b57e6da8ebfd638cd874
Tanmay Arya <tanmayarya80@gmail.com>
2022-07-25 23:11:04 +0530

    Recursion 3

---
+++ src/Recursion/Questions3.java
@@ -0,0 +1,44 @@
+package Recursion;
+
+public class Questions3 {
+	
+	public static void printReverse(int arr[], int idx) {
+		if(idx == arr.length) return;
+		
+		printReverse(arr, idx + 1);
+		System.out.println(arr[idx]);
+	}
+	
+	
+	public static int max(int arr[], int idx) {
+		if(idx == arr.length - 1) 
+			return arr[idx];
+		
+		int maxAhead = max(arr, idx + 1);
+		return Math.max(maxAhead, arr[idx]);
+	}
+	
+	public static int lastIndex(int arr[], int val, int idx) {
+		if(idx == arr.length) return -1;
+		
+		int ansFromArrayStartingAtNextIdx = lastIndex(arr, val, idx + 1);
+		
+		if(ansFromArrayStartingAtNextIdx == -1) {
+			if(arr[idx] == val) return idx;
+			else return -1;
+		} else { // ans is present
+			return ansFromArrayStartingAtNextIdx;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+
+		int arr[] = {1,1,1,2,3,4,5,6,7,8};
+//		printReverse(arr, 0);
+//		System.out.println(max(arr, 0));
+		System.out.println(lastIndex(arr, 1, 0));
+		
+	}
+
+}
